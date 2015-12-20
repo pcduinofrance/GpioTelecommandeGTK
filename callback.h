0bff4b7a5e1ba8ab40cf6f95c5ac51d3db703bb9
root <root@localhost.localdomain>
2015-12-20 23:56:34 +0100

    initial commit with apps

---
+++ callback.h
@@ -0,0 +1,14 @@
+void activate_cb0 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb1 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb2 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb3 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb4 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb5 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb6 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb7 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb8 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb9 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb10 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb11 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb12 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
+void activate_cb13 (GObject *switcher, GParamSpec *pspec, gpointer user_data);
